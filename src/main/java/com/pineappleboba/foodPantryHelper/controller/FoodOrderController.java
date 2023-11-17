ece924a1c332dfbaf1af78427132da32d11840b3
pineappleboba <luanasmith9@gmail.com>
2023-11-17 06:32:10 -0800

    add cross-origin header, restricted to just angular running on the same host

--- src/main/java/com/pineappleboba/foodPantryHelper/controller/FoodOrderController.java
+++ src/main/java/com/pineappleboba/foodPantryHelper/controller/FoodOrderController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/")
+@CrossOrigin(origins = "http://localhost:4200")
 public class FoodOrderController {
     private final FoodChoiceService foodChoiceService;
     private final FoodPickupService foodPickupService;
